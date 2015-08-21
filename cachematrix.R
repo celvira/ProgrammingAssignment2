fd8ad594b115ec578fe697fd3f710f21c51829aa
celvira28 <celvira28@gmail.com>
2015-08-21 09:54:31 +0200

    Programming Assignment 2. Version 1.0

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,46 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Set of functions related with matrix setting, and 
+## the computation of its inverse.
+## Assumming the matrix is a square invertible matrix
 
-## Write a short comment describing this function
+## 'makeCacheMatrix': define 4 functions related with a matrix
+## Input parameter 'x': must be a numeric matrix
+## Output parameter: list containing functions to
+##  1. 'set': set the matrix
+##  2. 'get': get the matrix
+##  3. 'setinv': set the inverse matrix
+##  4. 'getinv': get the inverse matrix
+## Example: A <- makeCacheMatrix(matrix(runif(9,0,5),3,3))
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  invmat <- NULL  # Inverse matrix null
+  set <- function (y) {
+    x <<- y
+    invmat <<- NULL
+  }
+  get <- function () as.matrix(x)
+  setinv <- function (A) invmat <<- A
+  getinv <- function () invmat
+  list(set = set, get = get, setinv = setinv, getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## 'cacheSolve': calculate the inverse matrix of A 
+## if the original matrix A has not changed.
+## Input parameter: an object created with makeCacheMatrix
+## Output parameter: the inverse matrix
+## Example: cacheSolve(A)
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  invmat <- x$getinv()
+  if (!is.null(invmat)) {
+    message("getting cached inverse matrix")
+    return(invmat)
+  }
+  mymatrix <- x$get()
+  ## Assumming square matrix and invertible
+  invmat <- solve(mymatrix)
+  ## Set the 'invmat' in the 'x' object
+  x$setinv(invmat)
+  ## Return a matrix that is the inverse of 'x'
+  invmat        
 }
